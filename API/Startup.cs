dd9643c160295f6540e645040bfc85fb54be1b20
Francisco <javiermartincuenca@gmail.com>
2020-03-15 00:42:46 +0100

    Error on 400 instead 404

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,51 +7,61 @@ using Microsoft.Extensions.Hosting;
 using Persistence;
 using MediatR;
 using Application.Activities;
+using FluentValidation.AspNetCore;
+using API.Middleware;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API
 {
   public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            //app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            app.UseCors("CorsPolicy");
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(opt =>
+      {
+        opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+      });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+      });
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddControllers().AddFluentValidation(cfg => 
+        cfg.RegisterValidatorsFromAssemblyContaining<Create>())
+        .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseMiddleware<ErrorHandlingMIddleware>();
+
+      if (env.IsDevelopment())
+      {
+        //app.UseDeveloperExceptionPage();
+      }
+
+      //app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+      app.UseCors("CorsPolicy");
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using MediatR;
 using Persistence;
@@ -20,12 +22,16 @@ namespace Application.Activities
       private readonly DataContext _context;
       public Handler(DataContext context)
       {
-          this._context = context;
+        this._context = context;
       }
 
       public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
       {
         var activity = await _context.Activities.FindAsync(request.Id);
+        if (activity == null)
+        {
+          throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found" });
+        }
         return activity;
       }
     }
